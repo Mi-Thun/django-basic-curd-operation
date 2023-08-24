916d36527a33a55097955c83838b6b6c469c60f7
Mithun <mithun@sgcsoft.net>
2023-08-24 15:42:02 +0600

    using html form add operation done

--- students/urls.py
+++ students/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('student/<str:name>', views.student),
+    path('add_student/', views.add_student, name='add_student'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import User
 
 def home(request):
     return HttpResponse("Hello World")
@@ -7,3 +8,16 @@ def home(request):
 
 def student(request, name):
     return HttpResponse(name)
+
+
+def add_student(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        
+        user = User(name=name, email=email)
+        user.save()
+
+        return redirect('/')
+    
+    return render(request, 'add.html')
